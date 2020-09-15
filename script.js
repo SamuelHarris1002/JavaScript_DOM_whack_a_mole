4da41cee16e71159ce26668bf3c5c2061df691c4
Samuel <sharris163@west-mec.org>
2020-09-15 12:59:05 -0700

    day 1

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 //step 1 select objects on screen & define globals//
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const countdownBoard = document.querySelector('.countdown');
+const startButton = document.querySelector('.startButton');
 
+let lastHole;
+let timeUp = false;
+let score = 0;
+let countdown;
+
+function pickRandomHole(holes) {
+    const randomHole = Math.floor(Math.random() * holes.length);
+    const hole = holes[randomHole];
+    if(hole === lastHole){
+        return(pickRandomHole(holes));
+    }
+    lastHole = hole;
+    return hole;
+}
 //step 2 add class 'up'//
+function popOut() {
+    const time = Math.random() * 1300 + 400;
+    const hole = pickRandomHole(holes);
+    hole.classList.add('up');
+    setTimeout(function(){
+        hole.classList.remove('up');
+        if(!timeUp) popOut();
+    }, time);
+}
+
 
 //step 3 dynamically change text content to countdown//
-
+function startGame() {
+    countdown = timeList/20;
+    scoreBoard.textContent = 0;
+    scoreBoard.getElementsByClassName.display = 'block';
+    countdownBoard.textContent = countdown;
+    timeUp = false;
+    score = 0;
+    popOut();
+    setTimeout(function(){
+        timeUp = true;
+    }, timeLimit );
+    let startCountdown = setInterval;we6
+}
 //step 4 adding event listeners//
 
 //step 5 keep track of score with forEach//
