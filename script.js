59f62d6f2430731f6a5c9396f134b715162be8e0
Samuel <sharris163@west-mec.org>
2020-09-15 15:23:20 -0700

    day 2

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let lastHole;
 let timeUp = false;
 let score = 0;
 let countdown;
+let timeLimit = 20000;
 
 function pickRandomHole(holes) {
     const randomHole = Math.floor(Math.random() * holes.length);
@@ -30,21 +31,41 @@ function popOut() {
     }, time);
 }
 
-
 //step 3 dynamically change text content to countdown//
 function startGame() {
-    countdown = timeList/20;
+    countdown = timeLimit/20;
     scoreBoard.textContent = 0;
     scoreBoard.getElementsByClassName.display = 'block';
     countdownBoard.textContent = countdown;
     timeUp = false;
     score = 0;
     popOut();
-    setTimeout(function(){
+    setTimeout(function() {
         timeUp = true;
     }, timeLimit );
-    let startCountdown = setInterval;we6
+    let startCountdown = setInterval(function(){
+        countdown -= 1;
+        countdownBoard.textContent = countdown;
+        if(countdown < 0) {
+            countdown = 0;
+            clearInterval(startCountdown);
+            countdownBoard.textContent = "Times up! Thank you for protecting our planet! This is the way!";
+        }
+    }, 1000);
 }
 //step 4 adding event listeners//
+startButton.addEventListener('click', startGame);
 
+function whack(e){
+    score++;
+    this.style.backgroundImage = 'url("yoda2.png")';
+    this.style.pointerEvents = 'none';
+    setTimeout(() => {
+        this.style.backgroundImage = 'url("yoda1.png)';
+        this.style.pointerEvents = 'all';
+    }, 800)
+    scoreBoard.textContent = score;
+}
 //step 5 keep track of score with forEach//
+moles.forEach(mole => mole.addEventListener('click', whack));
+popOut();
